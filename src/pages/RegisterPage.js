7e0e0cf78cfeb8bcebee0ad3284a1d830d1a3321
Jordane <jordane.frechet@gmail.com>
2020-03-03 16:39:01 +0100

    started register page

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Main from '../components/molecules/Main/Main';
+import './RegisterPage.scss';
 
 const RegisterPage = () => (
-  <Main page="register"><div>It works - Register</div></Main>
+  <Main page="register">
+    <section>
+      <div className="register-text">
+        <h3 className="content-title">Register</h3>
+      </div>
+    </section>
+
+  </Main>
 );
 
 export default RegisterPage;
